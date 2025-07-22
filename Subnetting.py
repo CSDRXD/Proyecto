26a74ffab3bc8f5c996bff2d0a770f8cfa4033ed
Carlos <carsanrindia@gmail.com>
2025-07-21 19:50:53 -0500

    Name of variables changed

--- Subnetting.py
+++ Subnetting.py
@@ -1,35 +1,35 @@
-oct1 = int(input("Ingrese su primer octeto: "))
-oct2 = int(input("Ingrese su segundo octeto: "))
-oct3 = int(input("Ingrese su tercer octeto: "))
-oct4 = int(input("Ingrese su cuarto octeto: "))
+oct1 = int(input("Enter the first octet: "))
+oct2 = int(input("Enter the second octet: "))
+oct3 = int(input("Enter the third octet: "))
+oct4 = int(input("Enter the fourth octet: "))
 
 if 1 <= oct1 <=126:
-    mod = "A"
+    ip_class = "A"
 elif 128 <= oct1 <=191:
-    mod = "B"
+    ip_class = "B"
 elif 192 <= oct1 <= 223:
-    mod = "C"
+    ip_class = "C"
 elif 224 <= oct1 <= 239:
-    mod = "D(Multicast)"
+    ip_class = "D(Multicast)"
 elif 240 <= oct1 <= 254:
-    mod = "E(Exp)"
+    ip_class = "E(Exp)"
 else:
-    mod = "Error"
+    ip_class = "Error"
 
-if mod == "A":
+if ip_class == "A":
     mask = "255.0.0.0"
-elif mod == "B":
+elif ip_class == "B":
     mask = "255.255.0.0"
-elif mod == "C":
+elif ip_class == "C":
     mask = "255.255.255.0"
 
 oct = mask.split(".")
 
-bits_red = sum(bin(int(o)).count("1") for o in oct)
-bits_host = 32 - bits_red
-hosts_red = 2**bits_host - 2
+network_bits = sum(bin(int(o)).count("1") for o in oct)
+host_bits = 32 - network_bits
+available_hosts = 2**host_bits - 2
 
-subn = bits_host **2
+subn = host_bits **2
 
 bin1 = bin(oct1)[2:].zfill(8)
 bin2 = bin(oct2)[2:].zfill(8)
@@ -37,10 +37,10 @@ bin3 = bin(oct3)[2:].zfill(8)
 bin4 = bin(oct4)[2:].zfill(8)
 
 print( "IP: ",oct1, oct2, oct3, oct4, 
-      "\nIP Binaria: ", bin1,
-      "\nMascara: ", mask,
-      "\nIP  clase: ", mod,
-          "\nBits de red: ", bits_red,
-          "\nBits de Host: ", bits_host,
-          "\nHosts Disponibles: ", hosts_red,
-          "\nSubredes Disponibles: ", subn, )
+      "\nIP Bin: ", bin1,
+      "\nMask: ", mask,
+      "\nIP  class: ", ip_class,
+          "\nNetwork Bits: ", network_bits,
+          "\nHost Bits: ", host_bits,
+          "\nAvalible hosts: ", available_hosts,
+          "\nsubnets avalibles: ", subn, )
